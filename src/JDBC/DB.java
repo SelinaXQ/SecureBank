6152a3c397dab02b798292d4325f66779e5e2293
SelinaXQ <xq_ecnu_sei@163.com>
2019-11-05 22:12:07 -0500

    db

--- src/JDBC/DB.java
+++ src/JDBC/DB.java
@@ -87,7 +87,7 @@ public class DB {
 				String stockName = pResultSet.getString(2);
 				Double stockPrice = pResultSet.getDouble(3);
 				res = new Stock(stockId, stockName, stockPrice);
-				//res.add
+
 			}
 
 		}catch (SQLException e) {
